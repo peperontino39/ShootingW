e0112db0dc7de14f247807ed28b7ee88b204e684
ニコニコしん\貴之 <hime1633004@yahoo.co.jp>
2017-10-08 21:55:47 +0900

    仮用リソース

---
+++ Assets/Script/Siciry/RayCastMouse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastMouse : MonoBehaviour {
+    public Camera _camera;
+    public float distance = 100f;
+    void Update()
+    {
+        // 左クリックを取得
+        if (Input.GetMouseButtonDown(0))
+        {
+            // クリックしたスクリーン座標をrayに変換
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction, new Color(1, 0, 0, 1));
+//             Rayの当たったオブジェクトの情報を格納する
+            RaycastHit hit = new RaycastHit();
+            // オブジェクトにrayが当たった時
+            if (Physics.Raycast(ray, out hit,distance))
+            {
+              
+                // rayが当たったオブジェクトの名前を取得
+                string objectName = hit.collider.gameObject.name;
+                Debug.Log(objectName);
+            }
+        }
+        
+    }
+
+   
+}
---
+++ Assets/Script/Siciry/SceneController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject hamada;
+    [SerializeField]
+    float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hamada.transform.localPosition += transform.right * speed * Time.deltaTime;
+    }
+}
