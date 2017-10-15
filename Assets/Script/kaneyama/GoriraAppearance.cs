988d4ad7370b84870eff6a1bd9c63f0659aed734
peperontino39 <peperrontino39@docomo.ne.jp>
2017-10-15 12:57:48 +0900

    メタファイル上げる

---
+++ Assets/Script/kaneyama/GoriraAppearance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ゴリラ登場クラス
+public class GoriraAppearance : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void AppearanceStart()
+    {
+        
+    }
+
+
+    IEnumerator Appearance()
+    {
+
+
+
+    }
+
+}
