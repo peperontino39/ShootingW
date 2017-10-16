36d000eb4ec850bf50ced2f03b8c37b3d117e4bf
ニコニコしん\貴之 <hime1633004@yahoo.co.jp>
2017-10-17 02:41:17 +0900

    うんこ投げコールバック

--- Assets/Script/Siciry/ThrowUnko.cs
+++ Assets/Script/Siciry/ThrowUnko.cs
@@ -5,33 +5,32 @@ using System;
 
 public class ThrowUnko : MonoBehaviour {
 
-    public Vector3 nawPosition;
+
     public Transform target;
     float time = 3;
     [SerializeField]
     AnimationCurve curve;
     float rotateDirection = -1; //-1時計回り1反時計回り
-
+    public Action callback = null;
     void Start()
     {
-        nawPosition = GameObject.Find("Boss").transform.position;
         target = GameObject.Find("Target").transform;
         throwStart();
-
     }
 
-    void throwStart(Action callback = null)
+    void throwStart()
     {
         var r = 0;
-        bool ttakai = nawPosition.y < target.position.y;
+        bool ttakai = transform.position.y < target.position.y;
         StartCoroutine(Easing.Tween(time, (t) =>
         {
-            var x = Mathf.Lerp(nawPosition.x, target.position.x, t);
-            var y = ttakai ? nawPosition.y + (-nawPosition.y + target.position.y) * curve.Evaluate(t) :
-            target.position.y + (-target.position.y + nawPosition.y) * curve.Evaluate(1 - t);
+            var x = Mathf.Lerp(transform.position.x, target.position.x, t);
+            var y = ttakai ? transform.position.y + (-transform.position.y + target.position.y) * curve.Evaluate(t) :
+            target.position.y + (-target.position.y + transform.position.y) * curve.Evaluate(1 - t);
 
-            transform.localPosition = new Vector3(x, y, Mathf.Lerp(nawPosition.z, target.position.z, t));
+            transform.localPosition = new Vector3(x, y, Mathf.Lerp(transform.position.z, target.position.z, t));
             transform.localRotation = Quaternion.Euler(0, 0, (r += 10) * rotateDirection);
         }, callback));
+      
     }
 }
